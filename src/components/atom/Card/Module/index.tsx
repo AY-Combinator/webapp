97fbc28117f68c52eecddca0d1fe669fc620ee30
Nikola Mitrovic <nikolaimitrovic@gmail.com>
2025-02-01 14:26:48 +0100

    feat(Module Card ): create module card reusable component

---
+++ src/components/atom/Card/Module/index.tsx
@@ -0,0 +1,44 @@
+import { Progress } from "@/components/ui/progress";
+import { CheckCircle, SketchLogo } from "@phosphor-icons/react/dist/ssr";
+
+interface ModuleCardProps {
+  title: string;
+  color: "golden" | "sky" | "orange" | "indigo";
+  icon: React.ReactNode;
+  points: number;
+}
+
+const ModuleCard = ({ title, color, icon, points }: ModuleCardProps) => {
+  const clampedPoints = Math.max(0, Math.min(points, 42));
+  const progressValue = (clampedPoints / 42) * 100;
+  return (
+    <div className="border border-solid border-black/30 shadow-sm shadow-black/15 rounded-sm bg-accent-foreground p-1 flex flex-col gap-1">
+      <div className="flex items-center gap-4 p-3">
+        <div
+          className={`size-16 bg-${color}-gradient rounded-lg border-2 border-solid border-black/15 flex items-center justify-center`}
+        >
+          {icon}
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-background text-lg font-archivo-black uppercase leading-none">
+            {title}
+          </h3>
+          <div className="flex items-center gap-3 text-[#9B9693]">
+            <div className="flex items-center gap-1">
+              <SketchLogo size={18} weight="light" />
+              <span className="text-sm">{clampedPoints} Points</span>
+            </div>
+            {progressValue === 100 && (
+              <div className="flex items-center gap-1">
+                <CheckCircle size={18} weight="fill" />
+                <span className="text-sm">Done</span>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <Progress value={progressValue} />
+    </div>
+  );
+};
+export default ModuleCard;
