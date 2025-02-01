2efd121bf2edb43e3ab60f6d9c2606f0b6b05ba1
Nikola Mitrovic <nikolaimitrovic@gmail.com>
2025-02-01 13:03:07 +0100

    feat(header): create header component
    Created a basic header component. It uses Logo and Navigation reusable

---
+++ src/components/molecule/Header/index.tsx
@@ -0,0 +1,17 @@
+import Logo from "@/components/atom/Logo";
+import Navigation from "@/components/atom/Navigation";
+import { DashboardNavigation } from "@/data/menus/DashboardNavigation";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0">
+      <div className="flex justify-between items-center gap-4 py-7">
+        <Logo />
+        <div className="flex gap-4 items-center">
+          <Navigation navigationItems={DashboardNavigation} />
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
