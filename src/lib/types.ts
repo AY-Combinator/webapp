fdf0ef9f38488c06239640b366049ef6a377f667
Nikola Mitrovic <nikolaimitrovic@gmail.com>
2025-02-01 12:17:24 +0100

    feat(types): define NavigationItem type

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+type NavigationItem = {
+  label: string;
+  href: string;
+  newTab?: boolean;
+  ariaLabel?: string;
+  external?: boolean;
+  tabIndex?: number;
+};
+
+export type { NavigationItem };
