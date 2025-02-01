743222d17773ac0cd5ff8927b5437cc509b7d40f
Nikola Mitrovic <nikolaimitrovic@gmail.com>
2025-02-01 12:06:40 +0100

    feat(logo component): create a logo atom component
    Create a logo atom component wrapped in link to homepage. The logo image

---
+++ src/components/atom/Logo/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import LogoImage from "@/assets/images/temporary-logo.svg";
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <Link href="/" aria-label="Go to Homepage" tabIndex={0}>
+      <Image
+        src={LogoImage}
+        alt="AY Combinator Logo"
+        width={154}
+        height={28}
+        priority
+      />
+    </Link>
+  );
+};
+export default Logo;
