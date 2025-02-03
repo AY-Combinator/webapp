a98cab8dcfcee9f08ed63bc848d21262b60c939d
Nikola <nikolaimitrovic@gmail.com>
2025-02-04 00:27:10 +0100

    feat(main sidebar): conditional back to dashboard button

--- src/components/molecule/Sidebar/Main/index.tsx
+++ src/components/molecule/Sidebar/Main/index.tsx
@@ -1,15 +1,31 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
 import ModuleList from "../../ModuleList";
+import { Button } from "@/components/ui/button";
+import { ArrowElbowDownLeft } from "@phosphor-icons/react/dist/ssr";
 
 const MainSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
-    <div className="flex h-full flex-col gap-5">
-      <div className="flex items-center justify-between gap-3">
-        <h2 className="text-lg text-background leading-none font-archivo-black">
-          Modules
-        </h2>
-        <span className="font-archivo text-lg text-background/30">4 / 8</span>
+    <div className="flex h-full flex-col gap-5 justify-between">
+      <div className="flex flex-col gap-5 h-full overflow-hidden">
+        <div className="flex items-center justify-between gap-3">
+          <h2 className="text-lg text-background leading-none font-archivo-black">
+            Modules
+          </h2>
+          <span className="font-archivo text-lg text-background/30">4 / 8</span>
+        </div>
+        <ModuleList />
       </div>
-      <ModuleList />
+      {pathname !== "/dashboard" && (
+        <Button
+          onClick={() => router.push("/dashboard")}
+          className="rounded-sm bg-accent-foreground text-background py-5 h-max hover:bg-orange/30 font-archivo font-medium"
+        >
+          <ArrowElbowDownLeft size={18} /> <span>Go Back to Dashboard</span>
+        </Button>
+      )}
     </div>
   );
 };
