6f61f2110dd3ad48cb2ca9c0a6add9f4724e09cb
Nikola <nikolaimitrovic@gmail.com>
2025-02-04 01:22:39 +0100

    feat(data room): create data room mock component

---
+++ src/components/molecule/DataRoom/index.tsx
@@ -0,0 +1,20 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { FoldersMockData } from "@/data/mock/folders";
+import Folder from "../Folder";
+import SectionWrapper from "@/components/atom/SectionWrapper";
+
+const DataRoom = () => {
+  return (
+    <SectionWrapper>
+      <h2 className="font-archivo-black text-lg leading-none">Data Room</h2>
+      <ScrollArea className="w-full">
+        <div className="flex flex-wrap gap-5">
+          {FoldersMockData.map((folder) => (
+            <Folder key={folder.title} {...folder} />
+          ))}
+        </div>
+      </ScrollArea>
+    </SectionWrapper>
+  );
+};
+export default DataRoom;
