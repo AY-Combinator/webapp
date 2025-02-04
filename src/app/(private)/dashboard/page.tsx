c57e0ec42ca2252742183bc5d71f4b70ef0a926c
Nikola <nikolaimitrovic@gmail.com>
2025-02-04 01:22:59 +0100

    feat(dashboard route): create dashboard mock route

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import DataRoom from "@/components/molecule/DataRoom";
+import Overview from "@/components/molecule/Overview";
+
 const DashboardRoute = () => {
-    return(
-        <div>
-            Dashboard
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col gap-6 overflow-hidden h-full">
+      <Overview />
+      <DataRoom />
+    </div>
+  );
+};
 export default DashboardRoute;
