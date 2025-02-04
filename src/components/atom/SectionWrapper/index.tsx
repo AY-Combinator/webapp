14ff0d1d9d2da4769d05a45ec91ad7bab80a5ca5
Nikola <nikolaimitrovic@gmail.com>
2025-02-04 01:22:04 +0100

    feat(section wrapper): create section wrapper reusable component

---
+++ src/components/atom/SectionWrapper/index.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const SectionWrapper = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <section
+      className={cn(
+        "flex p-6 flex-col gap-6 bg-white overflow-hidden border border-solid border-border shadow-lg w-full rounded",
+        className
+      )}
+    >
+      {children}
+    </section>
+  );
+};
+export default SectionWrapper;
