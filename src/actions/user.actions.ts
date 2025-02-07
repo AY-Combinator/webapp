2a3d6f92a5e3449fa0154db83ee50b153433879d
Nikola Mitrovic <nikolaimitrovic@gmail.com>
2025-02-07 17:38:04 +0100

    feat(server wallets): create server wallet on user creation

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -2,9 +2,15 @@
 
 import { User } from "@privy-io/react-auth";
 import prisma from "../../prisma/client";
+import { cookies } from "next/headers";
+import privy from "@/lib/privy";
 export const createUser = async (data: User) => {
   try {
     const userId = data.id.replace(/^did:privy:/, "");
+
+    const { id, address, chainType } = await privy.walletApi.create({
+      chainType: "ethereum",
+    });
 
     const newUser = await prisma.user.create({
       data: {
@@ -16,8 +22,15 @@ export const createUser = async (data: User) => {
             title: "",
           },
         },
+        Wallet: {
+          create: {
+            id: id,
+            address: address,
+            chainType: chainType,
+          },
+        },
       },
-      include: { projects: true },
+      include: { projects: true, Wallet: true },
     });
 
     return { success: true, user: newUser };
@@ -26,3 +39,18 @@ export const createUser = async (data: User) => {
     return { success: false, error: "Failed to create user." };
   }
 };
+
+export async function logoutUser() {
+  const cookieStore = await cookies();
+  cookieStore.delete("userId");
+}
+
+export async function getUserId() {
+  const cookieStore = await cookies();
+  const userId = cookieStore.get("userId")?.value;
+
+  if (!userId) {
+    return null;
+  }
+  return userId;
+}
