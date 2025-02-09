38530b64c62ed438a8c38d3412df6a61a0483e19
Petar Popovic <petarpnp@gmail.com>
2025-02-09 14:22:16 +0100

    feat(constants): add module to agent id mapping

--- constants.ts
+++ constants.ts
@@ -1,2 +1,30 @@
 export const AUTHONOME_URL = "https://autonome.alt.technology/agent-lqqwgl/agents'";
 export const POINTS_TO_UNLOCK_FUNDING = 1200;
+export const MODULE_TO_AGENT_ID = {
+  "problem-facing": "6bb0cefb-df38-040a-82c6-e67ba5dc2887",
+  "Venture Partner": "68bb428e-a72b-0597-8645-9ae25612f932",
+  "risks-and-hypothesis": "77b3f84c-e907-0798-ab91-5921fe7fd114",
+  "competitor-analysis": "44c3caca-8207-08ac-bb71-00c1c40f76cf",
+  "market-research": "dd43e5d4-973c-0dcd-a8aa-c975fdb25402",
+  "user-interviews": "e5c43c77-c0da-06f7-a259-69ea1d495b67",
+  "user-journey": "ecd4d47d-e1ab-0065-b0da-87439b075e80",
+  "user-persona": "08e7ee37-3faa-0f59-9da4-4583d6181b31",
+  "business-model": "e52a9766-e51a-032e-bc42-0c1bd33f3d56",
+  "investment-readiness": "3e1e2f8b-0b58-04c2-abf9-7b98dd6a3a82",
+  "exit-strategy": "2d49e152-543e-0113-b815-87812d6d900b",
+  "scaling-strategies": "9d98ca88-2910-071d-8c84-8a388effb812",
+  "pitch-prep": "4291ab6c-c725-0bd1-84fe-2d850f9a76c9",
+  "team": "53d14c0a-faf3-001d-91e0-46c217a68d59",
+  "measuring-impact-kpis": "5fe41c60-f8a6-0b4a-abc2-d48ffef0f9d9",
+  "sales": "043682b1-302e-00f9-8970-185210fcdde0",
+  "content-strategy ": "f62e9a20-770a-0a55-b0d0-6b11ab409ac2",
+  "community-building": "55a3014e-0c7f-046d-9c28-edb94966b83c",
+  "gtm": "6fd55ffe-0201-0076-9f75-d07a5cd3449e",
+  "brand-positioning": "174b3b8e-ecc8-0a4f-8bc7-a0828bfafccb",
+  "roadmapping": "263de32f-1ab5-0396-b274-c14029a40736",
+  "user-experience": "eb687163-6883-0dc2-9176-f48730251aca",
+  "risk-reevaluation": "2914746c-6a89-0939-b56d-085e83fe9cb8",
+  "tech-architecture": "9cc5a9d3-1bd7-06bc-9f2b-ed362c60ce83",
+  "mvp-development": "f027b976-87d8-0f6a-9596-77b7876940c0",
+  "jobs-to-be-done": "a93f4d41-cb57-0822-ae24-6f80395f12f2"
+};
