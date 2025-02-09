7bd62056874a79c621f61d1a1563338aa76fa049
Petar Popovic <petarpnp@gmail.com>
2025-02-09 15:43:35 +0100

    fix(leaderboard): add missing enableFundingButton

--- src/app/(private-view)/layout.tsx
+++ src/app/(private-view)/layout.tsx
@@ -1,14 +1,18 @@
+import { getUserProject } from "@/actions/project.actions";
 import Header from "@/components/molecule/Header";
+import { POINTS_TO_UNLOCK_FUNDING } from "../../../constants";
 
-export default function PrivateViewLayout({
+export default async function PrivateViewLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const project = await getUserProject();
+
   return (
     <div className="flex h-full min-h-screen">
       <div className="flex flex-col justify-between w-full h-auto ~px-6/10 bg-golden-sand-gradient">
-        <Header />
+        <Header enableFundingButton={(project?.project?.cumulativeProgress?.earnedPoints ?? 0) >= POINTS_TO_UNLOCK_FUNDING} />
         <main className="flex-1 w-full h-full">{children}</main>
       </div>
     </div>
